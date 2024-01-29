25f4c4fbed59204bd6e3f058c82e187344b7ebd2
Mamun <mamunurrashid.s.bd@gmail.com>
2024-01-30 02:32:38 +0600

    left side Nav just showing

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+    const [categories, setCategory] = useState([])
+    useEffect(() => {
+        fetch('/categories.json')
+            .then(res => res.json())
+            .then(data => setCategory(data))
+    }
+        , [])
     return (
-        <div>
-            <h1 className="text-3xl">Left side Nav</h1>
+        <div className="space-y-7">
+            <h1 className="text-xl font-semibold">All Category</h1>
+            {
+                categories.map(category => <NavLink
+                    key={category.id}
+                    to={`/category/${category?.id}`}
+                    className="block ml-4 text-[#9F9F9F] text-xl font-medium"
+                >{category.name}</NavLink>)
+            }
         </div>
     );
 };
